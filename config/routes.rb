6ab9a74069d5398c9f35c05d528d361159e3ec67
Ashish Garg <ashish.garg@headerlabs.com>
2017-05-21 17:48:11 +0530

    admi n chnages

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,8 @@ Rails.application.routes.draw do
   post '/cart', to: 'users#cart', as: 'user_cart'
   get '/cart', to: 'users#cart', as: 'show_user_cart'
   delete '/cart/:id', to: 'users#remove_cart_item', as: 'remove_cart_item'
-
   post '/order', to: 'users#order', as: 'order'
-
-
   post '/item/:id/item_variant', to: 'items#item_variant', as: 'item_variant_selection'
+
+  get 'about_us', to: 'static_pages#about_us', as: 'about_us'
 end
