142c460ca2af1ba660cf5571b2f3c7bda8297080
Ashish Garg <ashish.garg@headerlabs.com>
2017-05-21 18:45:47 +0530

    Css changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {:host => 'https://127.0.0.1'}
+  config.action_mailer.default_url_options = {:host => 'http://52.15.158.204'}
   config.middleware.delete Rack::Lock
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
