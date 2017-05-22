595dcf96d8e923a600ff8c4bea37bb46708fcb65
Ashish Garg <ashish.garg@headerlabs.com>
2017-05-22 09:41:11 +0530

    Mailer changes.

--- app/mailers/order_notifier_mailer.rb
+++ app/mailers/order_notifier_mailer.rb
@@ -3,7 +3,7 @@ class OrderNotifierMailer < ApplicationMailer
   default from: 'DrawingView'
 
   def notify_order_info(order)
-
+    @order = order
     mail to: order.user.email, subject: 'KapoorExports: Order places successfully.'
   end
 end
